6e06facf68d29c0dccab5b26af00737d95ffdec9
Sharon Byegon <byegon.sharon@gmail.com>
2024-03-27 07:55:26 +0300

    completed the assignment

--- index.js
+++ index.js
@@ -11,6 +11,18 @@ const tutorials = [
   'what is JSONP?'
 ];
 
+//takes an array of strings(tutorials) and returns a new array where each string has its first letter of each word uppercased.
+//various methods have been used to achieve this.
+// split method has been used to split the string into an array of words
+// map method has been used to iterate over each word in the array
+//charAt method has been used to get the first letter of each word
+//toUpperCase method has been used to uppercase the first letter of each word
+//slice method has been used to get the rest of the word
+//join method has been used to join the first letter of each word with the rest of the word
 const titleCased = () => {
-  return tutorials
+  return tutorials.map(tutorial => {
+    const words = tutorial.split(' ');
+    return words.map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ');
+  });
 }
+
